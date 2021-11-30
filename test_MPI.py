fe9d9eec8653d392bb2b286b0b1d6d2cc00b5256
afisarsy <afisarsy@gmail.com>
2021-12-01 03:20:30 +0700

    test update

--- test_MPI.py
+++ test_MPI.py
@@ -6,11 +6,11 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 def master():
-    print(rank)
+    print('master :', rank)
     data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
 def worker():
-    print(rank)
+    print('worker :', rank)
     data = np.zeros((3,3))
 
 def main():
@@ -18,7 +18,7 @@ def main():
     if rank == 0:
         master()
     else:
-        worker
+        worker()
 
 if __name__ == '__main__':
     main()
